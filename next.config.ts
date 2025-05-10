83938d8025a0d8732f06bc4d8790ced12bdfbb9d
SSBFDK <felipe.don.joao.repker@scania.com>
2025-05-10 18:00:21 -0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Disable type checking during build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
